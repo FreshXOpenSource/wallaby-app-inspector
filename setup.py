e81bc909872159a330c0bf18ed931992e0671f37
Dominik Roessler <dominik@freshx.de>
2013-02-11 14:39:32 +0100

    Enhanced viewer for widget lsit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='wallaby-app-inspector',
-      version='0.1.23',
+      version='0.1.24',
       url='https://github.com/FreshXOpenSource/wallaby-app-inspector',
       author='FreshX GbR',
       author_email='wallaby@freshx.de',
--- wallaby/apps/inspector/rooms/__config__.py
+++ wallaby/apps/inspector/rooms/__config__.py
@@ -6,19 +6,34 @@ from wallaby.pf.room import *
 from wallaby.pf.peer.viewer import Viewer
 from wallaby.pf.peer.tab import Tab
 
+from wallaby.pf.peer.multiViewQuery import MultiViewQuery
+from wallaby.pf.peer.documentListQuery import DocumentListQuery
+
 class __config__(Room):
     def __init__(self, name):
         Room.__init__(self, name)
 
         self._widgetViewer = None
         self._roomViewer = None
+        self._allRooms = None
+        self._multiQuery = None
+        self._docList = None
 
         self._initialized = False
         self._state = None
+        self._lastSel = None
 
     def customPeers(self):
         if self._widgetViewer is not None: self._widgetViewer.destroy()
         if self._roomViewer is not None: self._roomViewer.destroy()
+        if self._allRooms is not None: self._allRooms.destroy()
+        if self._multiQuery is not None: self._multiQuery.destroy()
+        if self._docList is not None: self._docList.destroy()
+
+        self._multiQuery = MultiViewQuery("__CONFIG__", pillow=None)
+        self._docList = DocumentListQuery("__CONFIG__")
+
+        self._allRoom = Viewer("__CONFIG__", self._roomsChanged, "rooms", raw=True)
 
         self._widgetViewer = Viewer("__CONFIG__", self._widgetSelected, "widgets.*", raw=True, exact=True)
         self._roomViewer = Viewer("__CONFIG__", self._roomSelected, "rooms.*", raw=True, exact=True)
@@ -28,6 +43,7 @@ class __config__(Room):
             self.catch("Inspector.In.RemovePeer", self._removePeer)
             self.catch("Inspector.In.Suggest", self._doSuggest)
             self.catch("Inspector.In.InitializeRoom", self._initRoom)
+            self.catch("MultiViewer.Out.Select", self._multiViewerChanged)
 
             from wallaby.pf.peer.editDocument import EditDocument
             self.catch(EditDocument.Out.State, self._newState)
@@ -35,11 +51,43 @@ class __config__(Room):
             from wallaby.common.document import Document
             from wallaby.pf.peer.credentials import Credentials
             House.get("__DOC__").throw(Credentials.Out.Credential, Document())
+            House.get("__FILTERS__").throw(Credentials.Out.Credential, Document())
 
         self._initialized = True
         self._lastSelection = None
         self._lastRoom = None
         self._suggests = None
+
+    def _multiViewerChanged(self, pillow, sel):
+        if sel is None or len(sel) < 1: return
+
+        if self._lastSel != sel:
+            self._lastSel = sel
+            self.throw("DocumentChanger.In.Select", ("widgets", sel[0]))
+
+    def _roomsChanged(self, rooms):
+        if rooms is None: return
+
+        import wallaby.FXUI as FXUI
+        if FXUI.mainWindow.options().app != "inspector":
+            filters = {
+                "filters": [
+                    ["All", [{"__key__": "!inspector-"}, {"__key__": "!itest-"}]]
+                ]
+            }
+            for room in sorted(rooms.keys()):
+                filters["filters"].append([room, [{"room": "^" + room}]])
+        else:
+            filters = {
+                "filters": [
+                    ["All", []],
+                    ["Inspector", [{"__key__": "^inspector-"}]],
+                    ["Inspector-Test", [{"__key__": "^itest-"}]]
+                ]
+            }
+
+        from wallaby.common.document import Document
+        House.get("__FILTERS__").throw(Viewer.In.Document, Document(data=filters))
 
     def _newState(self, pillow, state):
         self._state = state
